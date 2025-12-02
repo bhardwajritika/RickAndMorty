29b98a2c53dd2448ece241b6ad57c884b1145305
Ritika <bhardwajritika11@gmail.com>
2025-12-02 23:11:48 +0530

    Compositional Layout of the character detail view

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 /// Controller to show info about single character
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
+    
+    private let detailView : RMCharacterDetailView
 
+    // MARK: - Init
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -26,7 +30,54 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(didTapShare))
+        addConstraints()
+        
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
+    }
+    
+    @objc
+    private func didTapShare() {
+        // Share character info
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
 
+}
+
+// MARK: - For CollectionView
+extension RMCharacterDetailViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int ) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if indexPath.section == 0  {
+            cell.backgroundColor = .systemBlue
+        } else if indexPath.section == 1 {
+            cell.backgroundColor = .systemYellow
+        } else {
+            cell.backgroundColor = .systemGreen
+        }
+        return cell
+    }
 }
