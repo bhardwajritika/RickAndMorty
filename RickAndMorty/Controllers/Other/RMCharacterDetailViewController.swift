79da7425040a9301eb7c72e27790aa8e88b6ad0d
Ritika <bhardwajritika11@gmail.com>
2025-12-10 21:32:46 +0530

    Add RMEpisode detail screen

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -101,4 +101,18 @@ extension RMCharacterDetailViewController : UICollectionViewDelegate, UICollecti
             return cell
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let sectionType = viewModel.sections[indexPath.section]
+        switch sectionType {
+        
+        case .information, .photo :
+            break
+        case .episodes:
+            let episodes = self.viewModel.episodes
+            let selection = episodes[indexPath.row]
+            let vc = RMEpisodeDetailViewController(url: URL(string: selection))
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
---
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RMEpisodeDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Tarun Sharma on 10/12/25.
+//
+
+import UIKit
+
+
+/// VC to show detail about single episode
+final class RMEpisodeDetailViewController: UIViewController {
+    
+    private let url: URL?
+    
+    // MARK: - Init
+    
+    init(url: URL?) {
+        self.url = url
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Episode"
+        view.backgroundColor = .systemGreen
+
+    }
+  
+
+}
